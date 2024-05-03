baae1f25afad0ba6d4a507a871f74cfb3760049f
santicasalis <santicasalis@hotmail.com>
2024-05-03 17:05:46 -0300

    noticias

---
+++ secciones/noticias/script.js
@@ -0,0 +1,135 @@
+let noticiasJson = [
+  {
+    id: 1,
+    titulo:
+      "Atenti Argentina: el jueves conocerá a su tercer rival en los Juegos Olímpicos",
+    categoria: "JUEGOS OLÍMPICOS",
+    resumen:
+      "Indonesia e Irak jugarán por el tercer puesto en el Preolímpico de Asia. El ganador irá a la zona de la Selección de Mascherano.",
+    contenido:
+      "El pasado 20 de marzo se sortearon los grupos para el certamen masculino de fútbol de los Juegos Olímpicos. También para el femenino. Argentina estará presente en el primero, encabezando el grupo B con Marruecos, Ucrania y un rival a definir desde Asía. El equipo de Javier Mascherano conocerá quién será el tercero el próximo jueves. Saldrá del partido por el tercer puesto que jugarán Irak e Indonesia en el Preolímpico asiático.Indonesia cayó en semis por 2-0 ante Uzbekistán, que jugará la final con Japón. Los nipones vencieron 2-1 a los iraquíes. Los ganadores ya aseguraron su boleto a la cita olímpica. Estarán acompañados por el tercero. Quien gane la final irá al grupo D (Paraguay, Mali e Israel) y el segundo al C (España, Egipto y República Dominicana).",
+    imagen:
+      "https://www.ole.com.ar/images/2024/04/29/P9hfIN2a__1290x760__1.jpg",
+  },
+  {
+    id: 2,
+    titulo:
+      "El handball de Argentina ya conoce sus días y horarios para los Juegos Olímpicos",
+    categoria: "HANDBALL",
+    resumen:
+      "Se sorteó el fixture y Los Gladiadores debutarán contra Noruega, dos veces subcampeón del mundo. Todos los partidos de la Albiceleste.",
+    contenido:
+      "La potente selección de Noruega será el primer rival de Argentina en el torneo masculino de handball de los Juegos Olímpicos de París 2024, según el calendario anunciado este lunes por la Federación Internacional de Balonmano (IHF). Los escandinavos ya habían derrotado a Los Gladiadores por 27-23 en los últimos JJOO de Tokio en 2021. La Albiceleste, que forma parte del grupo B, se estrenará en su cuarto participación consecutiva en Juegos Olímpicos ante el conjunto nórdico, dos veces subcampeón mundial (2017 y 2019), el sábado 27 de julio a partir de las 11, hora argentina, en una primera jornada en la que los dos grandes duelos serán los choques Alemania-Suecia y Dinamarca-Francia.Según los calendarios publicados este lunes por la IHF desde que comienza el torneo femenino el día 25 hasta el 4 de agosto, con la excepción del 26 debido a la Ceremonia de Inauguración de los Juegos, se jugarán seis partidos por jornada en el Arena 6 Sur de París, escenario de la primera fase.",
+    imagen:
+      "https://www.ole.com.ar/images/2024/04/29/WI9mAhc9D_1290x760__1.jpg",
+  },
+  {
+    id: 3,
+    titulo:
+      "Estados Unidos confirmó una lista repleta de estrellas para los Juegos Olímpicos",
+    categoria: "JUEGOS OLÍMPICOS",
+    resumen:
+      "La selección norteamericana se prepara para competir en los JJ.OO. de París 2024. Otro Dream Team.",
+    contenido:
+      "Estados Unidos confirmó la lista definitiva para competir en los Juegos Olímpicos de París 2024. Si bien a principios del 2024 la Federación de Básquet de Estados Unidos (USA Basketball) ya había dado a conocer un listado de 41 jugadores preseleccionados con LeBron James y Stephen Curry a la cabeza, este miércoles se confirmaron los 12 players que irán en busca del oro. Vale destacar que, recién en Barcelona 1992, los norteamericanos comenzaron a competir con jugadores NBA.",
+    imagen: "https://www.ole.com.ar/images/2018/02/14/SyaFa3-PM_860x575__1.jpg",
+  },
+  {
+    id: 4,
+    titulo: "Buenas noticias para Los Pumas 7's: Moneta llega a París 2024",
+    categoria: "RUGBY",
+    resumen:
+      "Gómez Cora dijo que, pese a la lesión que necesitó operación, el Rayo llega a los Juegos, aunque quedó la mala noticia para el Circuito.",
+    contenido:
+      "Da gusto escuchar a Santiago Gómez Cora. El entrenador de seven, un apasionado del juego, da lecciones cada vez que habla de táctica, selección de jugadores y, como es este caso, cuando da buenas noticias. El responsable de Los Pumas 7's, uno de los firmes candidatos de la delegación argentina a buscar un podio en los Juegos Olímpicos, habló de la figura del seleccionado, Marcos Moneta, y entregó un buen pronostico.",
+    imagen:
+      "https://www.ole.com.ar/images/2023/12/04/nHogDnUau_1290x760__1.jpg",
+  },
+  {
+    id: "5",
+    titulo: "Juegos Olímpicos París 2024: Argentina sigue sumando clasificados",
+    categoria: "JUEGOS OLÍMPICOS",
+    resumen:
+      "Daiana Ocampo finalizó 7º en la Maratón de Hamburgo con un tiempo de 2h26m24s y consiguió la marca para ir a la cita olímpica.",
+    contenido:
+      "Faltan 89 días para el inicio de los Juegos Olímpicos. Ahora bien, mientras la organización sigue inaugurando sedes y definiendo los detalles que quedan para la cita internacional y la llama está pronta a su arribo a Francia, los deportistas argentinos continúan clasificándose para estar en París 2024. En efecto, la delegación nacional sumó un nuevo nombre a la extensa lista que ya incluye 104 atletas: Daiana Ocampo sacó un ticket para estar en la gran cita del deporte, tras haber terminado en la séptima posición en la tradicional maratón de Hamburgo, donde completó el recorrido con un tiempo de 2h26m24s (su mejor marca). Allí, cruzó la meta detrás de cuatro keniatas y dos atletas etíopes.",
+    imagen:
+      "https://www.ole.com.ar/images/2024/04/28/Du1vTPXje_1290x760__1.jpg",
+  },
+  {
+    id: "6",
+    titulo:
+      "Julián Gutiérrez, el nuevo clasificado argentino a los Juegos Olímpicos",
+    categoria: "JUEGOS OLÍMPICOS",
+    resumen:
+      "Logró la clasificación tras haber conquistado el Campeonato de las Américas de Tiro en la categoría de rifle de aire de diez metros.",
+    contenido:
+      "Se alarga la lista de argentinos para los Juegos Olímpicos de París 2024. Cada vez son más los deportistas que tendrán el honor de representar a la Argentina en el evento multidisciplinario. Ahora se suma Julián Gutiérrez, luego de haberse consagrado campeón en la categoría de rifle de aire de diez metros en el Campeonato de las Américas de Tiro.",
+    imagen:
+      "https://www.ole.com.ar/images/2024/04/05/jEY6j6HUI_1290x760__1.jpg",
+  },
+  {
+    id: "7",
+    titulo:
+      "Nadal dijo que planea jugar dobles con Alcaraz en los Juegos de París",
+    categoria: "JUEGOS OLÍMPICOS",
+    resumen: "El multicampeón español ya tiene dos medallas olímpicas",
+    contenido:
+      "El tenista español Rafael Nadal dijo el viernes que si no tiene ningún problema físico disputará  la prueba de dobles en los Juegos de París con Carlos Alcaraz, y remarcó que le gustaría que puedan participar juntos en algún torneo previo a la cita olímpica.Alcaraz había dicho esta semana en una entrevista con el diario Marca que no se había atrevido a preguntarle a Nadal si jugarían juntos en París.No tiene que preguntarme nada, si no hay nada extraño jugaremos. Es una gran ilusión para mí y si no estoy equivocado me han dicho que para él también, dijo Nadal en las redes sociales del Abierto de Madrid.Sería bonito poder jugar algún torneo antes de los Juegos para prepararnos, para entendernos, una oportunidad de hacer una buena pareja y aspirar a cosas bonitas. Sería algo bueno para los dos y para el equipo español, sostuvo Nadal.",
+    imagen:
+      "https://imagenes.heraldo.es/files/image_990_556/uploads/imagenes/2024/05/01/madrid-open-1.jpeg",
+  },
+
+  {
+    id: "9",
+    titulo:
+      "¡Cuatro nuevos clasificados argentinos a los Juegos Olímpicos de París 2024!",
+    categoria: "JUEGOS OLÍMPICOS",
+    resumen:
+      "A falta de cuatro meses del inicio, se sumaron más atletas a la delegación que estará en la capital francesa.",
+    contenido:
+      "Faltan poco más de cuatro meses para que se encienda la antorcha olímpica en París, más precisamente, en el Sena. Allí, en el emblemático río de la capital francesa, se realizará el 26 de julio la ceremonia de apertura de esta nueva edición de los Juegos Olímpicos. Será la primera vez que esta celebración se festeje fuera de un estadio, en donde el público podrá saludar a los atletas mientras desfilan en barcos flotantes.",
+    imagen:
+      "https://www.ole.com.ar/images/2024/03/16/WV-ipqBDa_1290x760__1.jpg",
+  },
+  {
+    id: "10",
+    titulo:
+      "El cruce de fechas que complica la presencia de Messi en los Juegos Olímpicos",
+    categoria: "JUEGOS OLÍMPICOS",
+    resumen:
+      "Inter Miami debuta en la Leagues Cup durante el torneo en Francia. ¿Qué hará Leo?",
+    contenido:
+      "Hay una ilusión y al mismo tiempo una realidad que se da de frente con el calendario. El fútbol no para y, entonces, Lionel Messi no para. Pero tampoco es que puede estar o jugar dos torneos el mismo tiempo con dos camisetas distintas. Por eso, resulta muy complicada su presencia en los Juegos Olímpicos 2024.",
+    imagen:
+      "https://www.ole.com.ar/images/2024/03/14/5kEVE4X42_1290x760__1.jpg",
+  },
+];
+
+function cargarNoticias() {
+  var noticiasContainer = document.getElementById("noticias");
+
+  noticiasJson.forEach(function (noticia) {
+    let noticiaContainer = document.createElement("div");
+    noticiaContainer.classList.add("noticiaContainer");
+    noticiaContainer.setAttribute("style", "width: 300px; height: 300px;");
+
+    let categoria = document.createElement("p");
+    categoria.textContent = noticia.categoria;
+
+    let imagen = document.createElement("img");
+    imagen.src = noticia.imagen;
+    imagen.alt = "imagen";
+    imagen.setAttribute("style", "width: 300px; height: auto;");
+
+    let titulo = document.createElement("h3");
+    titulo.textContent = noticia.titulo;
+
+    noticiaContainer.appendChild(categoria);
+    noticiaContainer.appendChild(imagen);
+    noticiaContainer.appendChild(titulo);
+    noticiasContainer.appendChild(noticiaContainer);
+  });
+}
+
+window.onload = cargarNoticias;
